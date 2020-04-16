a357ef54af1a7bb0be3d417f6a6182ac58be1943
Felipe Pedrosa <felipeepedrosa@gmail.com>
2020-04-16 00:03:24 -0300

    Desafio fundamentos em NodeJS

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,13 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('O valor da operação ultrapassa o limite existente');
+    }
+    return this.transactionsRepository.create({ title, type, value });
   }
 }
 
